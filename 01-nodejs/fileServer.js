3d2231e0c946ad4e04214666b3c545b5e1e86a83
Avinash <avinashkumarofficial95@gmail.com>
2026-03-20 12:57:35 +0530

    feat: implement file server with endpoints for listing files and retrieving file content

--- 01-nodejs/fileServer.js
+++ 01-nodejs/fileServer.js
@@ -12,10 +12,36 @@
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
  */
+const { error } = require('console');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const FILES_DIR = path.join(__dirname, "files");
 
+app.get("/files", (req,res)=>{
+  fs.readdir(FILES_DIR, (err,files)=>{
+    if(err){
+      return res.status(500).json({error: "Unable to read directory"});
+    }
+    return res.status(200).json(files);
+  })
+})
 
+app.get("/file/:filename", (req,res)=>{
+  const filename = req.params.filename;
+  const filePath = path.join(FILES_DIR,filename);
+
+  fs.readFile(filePath,"utf-8", (err,data)=>{
+    if(err){
+      return res.status(404).send("File not found");
+    }
+
+    return res.status(200).send(data);
+  })
+})
+
+app.use((req,res)=>{
+  return res.status(404).send("Route not found")
+})
 module.exports = app;
